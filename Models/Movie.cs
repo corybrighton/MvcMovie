a29db71766e8354e83365cc675cd688957ea8cfa
Cory Brighton <corysairplane@gmail.com>
2019-05-09 13:39:56 -0600

    Commit

---
+++ Models/Movie.cs
@@ -0,0 +1,21 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MvcMovie.Models
+{
+  public class Movie
+  {
+    public int Id { get; set; }
+    public string Title { get; set; }
+
+    [Display(Name = "Released Date")]
+    [DataType(DataType.Date)]
+    public DateTime ReleasedDate { get; set; }
+    public string Genre { get; set; }
+
+    [Column(TypeName = "decimal(18,2)")]
+    public decimal Price { get; set; }
+    public string Rating { get; set; }
+  }
+}
